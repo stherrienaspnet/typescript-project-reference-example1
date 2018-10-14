f51de4394f0d4725ed781cf3ac5148aa6bd458e3
Stephane <stephane.therrien@parker.com>
2018-10-14 09:27:35 -0400

    Removed commun outDir for all project

--- shared-lib/src/physical-type/physical-type.service.ts
+++ shared-lib/src/physical-type/physical-type.service.ts
@@ -1,4 +1,4 @@
-import {IPhysicalType} from "../../../dist/src/physical-type/physical-type.interface";
+import {IPhysicalType} from "../../../shared-type/dist/src/physical-type/physical-type.interface";
 
 export interface IPhysicalTypeService
 {
